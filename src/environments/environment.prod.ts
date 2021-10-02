6195286f97fe10c5f17e7b005114abd99a9758a9
Facundo Araujo <facundoaraujo14@gmail.com>
2021-10-02 14:33:04 -0300

    remove files

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  urlServices: 'https://randomuser.me/api',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  urlServices: 'https://randomuser.me/api',
-};
